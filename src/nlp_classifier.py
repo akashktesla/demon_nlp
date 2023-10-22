beb18ef7fec364c625669140163fb60f461fa7ec
akashktesla <akashktesla@gmail.com>
2023-10-22 18:31:39 +0530

    :)

--- src/nlp_classifier.py
+++ src/nlp_classifier.py
@@ -1,15 +1,26 @@
-##import
-#import spacy
-##load the model
-#nlp = spacy.load("en_core_web_md")
-#text = "+(a,b)"
+#import
+import spacy
+#load the model
+nlp = spacy.load("en_core_web_md")
+text = "+(a,b)"
+doc = nlp(text)
+x = {}
+x1 = []
+for token in doc:
+    # print(token.text, token.vector)
+    x[token.text] = token.vector
+    x1.append(token.vector)
 
-#doc = nlp(text)
+# print(f"x: {len(x1)}")
+y = [0,1,2,1,2,1]
 
-#for token in doc:
-#    print(token.text, token.vector)
-
+#classifier
 from sklearn.svm import SVC
 
-classifier = SVC(kernel = "linear")
-print(classifier)
+classifier = SVC(kernel = "rbf")
+classifier.fit(x1,y)
+
+text = "+( a,b)"
+doc = nlp(text)
+for token in doc:
+    print(token.text,classifier.predict([token.vector]))
