28139dc4a2505db4ad2e1563f85226a3e8e9ded3
akashktesla <akashktesla@gmail.com>
2023-10-23 01:20:52 +0530

    :)

--- src/nlp_classifier.py
+++ src/nlp_classifier.py
@@ -1,25 +1,35 @@
-#classifier
+import spacy
 from sklearn.svm import SVC
 import pandas as pd
-import ast
+from data_builder import build_data
+from joblib import dump, load
+
+nlp = spacy.load("en_core_web_md")
+def main():
+    #return classifier by training the model
+    # clf = train_clf_model()
+    #save data
+    # dump(clf,'models/math_classifier.joblib')
+
+    clf = load('models/math_classifier.joblib')
+    test_clf_model(clf)
 
 
-#importing data
-df = pd.read_csv("dataset/math_dataset_proc.csv")
-x = df['x'].to_numpy()
-for i in range(len(x)):
-    print(x[i])
-    x[i] = x[i].to_numpy()
-y = df['y'].to_numpy()
+#to train the classifier
+def train_clf_model():
+    #build data builds the data from the csv gets the word vector and returns x,y
+    x,y = build_data("dataset/math_dataset.csv")
+    #classifier with rbf kernel
+    clf = SVC(kernel = "rbf")
+    #training
+    clf.fit(x,y)
+    return clf
 
-print(x)
-# print(y)
+def test_clf_model(clf):
+    text = "+( c, b )"
+    doc = nlp(text)
+    for token in doc:
+        print(token.text,clf.predict([token.vector]))
 
-
-# classifier = SVC(kernel = "rbf")
-# classifier.fit(x,y)
-
-# text = "+( c, b )"
-# doc = nlp(text)
-# for token in doc:
-#     print(token.text,classifier.predict([token.vector]))
+if __name__ =="__main__":
+    main()
