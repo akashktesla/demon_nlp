222a7ff15dd14c06127910d543793c4b58d7ce15
akashktesla <akashktesla@gmail.com>
2023-10-23 13:38:13 +0530

    :)

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use pyo3::types::{PyTuple, PyList};
 use pyo3::prelude::*;
 
 fn main(){
-    let asn_code = include_str!("/home/aki/projects/projectD/demon_nlp/src/asn.py");
+    let asn_code = include_str!("/home/aki/projects/projectD/demon_nlp/src/nlp_classifier.py");
+    // println!("{}",asn_code);
 
     Python::with_gil(|py|{
-
-        let asn = PyModule::from_code(py, asn_code, "", "").unwrap();
-        let args = PyTuple::new(py, &["I love apples"]);
-        let a = asn.getattr("extract_dependency").unwrap().call1(args).unwrap();
-        println!("out: {:?}",a);
-        // let b:HashMap<String,Vec<String>> = a.extract().unwrap();
-
+        let nlp_clf = PyModule::from_code(py, asn_code, "", "").unwrap();
+        let args = PyTuple::new(py, &["models/math_classifier.joblib"]);
+        nlp_clf.getattr("test_clf_model").unwrap().call1(args).unwrap();
     });
 }
+
+
+
 
 
--- src/nlp_classifier.py
+++ src/nlp_classifier.py
@@ -1,18 +1,54 @@
 import spacy
 from sklearn.svm import SVC
 import pandas as pd
-from data_builder import build_data
 from joblib import dump, load
 
 nlp = spacy.load("en_core_web_md")
+
 def main():
     #return classifier by training the model
     # clf = train_clf_model()
     #save data
     # dump(clf,'models/math_classifier.joblib')
 
-    clf = load('models/math_classifier.joblib')
-    test_clf_model(clf)
+    test_clf_model('models/math_classifier.joblib')
+
+def build_data(path):
+    #load a csv file
+    df = pd.read_csv(path)
+    x_temp = df["x"].to_numpy()
+    y_temp = df["y"].to_numpy()
+
+    x = []
+    y = []
+
+    print(y_temp)
+    for i in y_temp:
+        y_split = i.split(',')
+        for j in y_split:
+            y.append(j)
+
+    for i in x_temp:
+        # print(f"i: {i}")
+        doc = nlp(i)
+        for token in doc:
+            # print(f"token_text: {token.text}")
+            #appending the token word vector
+            x.append(token.vector)
+
+    # print(x)
+    # print(f"x: {x}")
+    # print(f"y: {y}")
+    # print(df)
+    return (x,y)
+
+
+
+#-----saving------
+#creating a dataframe
+# data = {'x':x, 'y': y}
+# df = pd.DataFrame(data)
+# df.to_csv('dataset/math_dataset_proc.csv')
 
 
 #to train the classifier
@@ -25,11 +61,14 @@ def train_clf_model():
     clf.fit(x,y)
     return clf
 
-def test_clf_model(clf):
+def test_clf_model(path):
+    clf = load(path)
     text = "+( c, b )"
     doc = nlp(text)
     for token in doc:
         print(token.text,clf.predict([token.vector]))
+
+
 
 if __name__ =="__main__":
     main()
